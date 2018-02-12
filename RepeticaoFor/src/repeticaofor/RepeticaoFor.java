11d3944946e643810c601978dbb14f5fba8b4ab5
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-02-11 20:40:50 -0400

    Repetição for

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author wellington
+ */
+public class RepeticaoFor {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		for (int cc = 0; cc <= 10; cc += 2) {
+			System.out.println("Cambalhota " + cc);
+		}
+		for (int i = 0; i <= 100; i += 10) {
+			System.out.println(i);
+		}
+	}
+	
+}
